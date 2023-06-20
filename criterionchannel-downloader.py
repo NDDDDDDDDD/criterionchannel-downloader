2e1a1125d89c18803f8aceacb0f1f59eb61d5fc1
NDDDDDDDDD <102587802+NDDDDDDDDD@users.noreply.github.com>
2023-06-20 00:21:51 +0000

    Update criterionchannel-downloader.py

--- criterionchannel-downloader.py
+++ criterionchannel-downloader.py
@@ -2,14 +2,34 @@ import requests
 import subprocess
 import re
 import base64
-filename = input("Enter final filename: ")
-licenseurl = input("Enter license url: ")
-mpd = input("Enter MPD url: ")
+import time
+import json
+import os
 
-folder_path = f"ADDHERE" # make this a valid path, files will be downloaded to this location
-dest_dir = f"{folder_path}/{filename}"
+mainurl = input("Enter config url: ")
+response = requests.get(f'{mainurl}')
+data=json.loads(response.text)
+license_url = data["request"]["drm"]["cdms"]["widevine"]["license_url"]
+mpd = data['request']['files']['dash']['cdns']['fastly']['avc_url']
+
+license_1url = requests.get(f'{license_url}')
+final_license = license_1url.text
+
+filename = input("Enter file/folder name: ")
+folder_path = f"A:/criterion"
 
 kid = requests.get(mpd)
+os.makedirs(folder_path, exist_ok=True)
+try:
+    text_track_url = data["request"]["text_tracks"][0]["url"]
+    subs = requests.get(f'{text_track_url}')
+    subs = subs.text
+    os.makedirs(os.path.join(folder_path, filename), exist_ok=True)
+    
+    with open(os.path.join(folder_path, filename, f"{filename}.en.srt"), "w", encoding="utf-8") as file:
+        file.write(subs)
+except KeyError:
+    print("No external subtitles, they might be hardcoded.")
 result = re.search(r'cenc:default_KID="(\w{8}-(?:\w{4}-){3}\w{12})">', str(kid.text))
 def get_pssh(keyId):
     array_of_bytes = bytearray(b'\x00\x00\x002pssh\x00\x00\x00\x00')
@@ -18,12 +38,11 @@ def get_pssh(keyId):
     array_of_bytes.extend(bytes.fromhex(keyId.replace("-", "")))
     return base64.b64encode(bytes.fromhex(array_of_bytes.hex()))
 
-# Extract the encryption key ID from the regular expression match and generate the PSSH box
 kid = result.group(1).replace('-', '')
 assert len(kid) == 32 and not isinstance(kid, bytes), "wrong KID length"
 pssh = format(get_pssh(kid).decode('utf-8'))
 json_data = {
-    'license': f'{licenseurl}',
+    'license': f'{final_license}',
     'headers': '',
     'pssh': f'{pssh}',                                                
     'buildInfo': '',                                                 
@@ -31,7 +50,6 @@ json_data = {
     'cache': False,                                                    
 }
 
-# Send a POST request with the headers and JSON data to the specified URL
 response = requests.post('https://cdrm-project.com/wv', json=json_data)
 result = re.search(r"[a-z0-9]{16,}:[a-z0-9]{16,}", str(response.text))
 decryption_key = result.group()
